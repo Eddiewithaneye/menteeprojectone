6c9c4f015a16386b47bcf0b18560d5b7b9862758
Eddiewithaneye <eddiewithaneye.ps@gmail.com>
2025-09-26 00:15:00 -0400

    feat: create AboutMe component structure

---
+++ src/AboutMe.js
@@ -0,0 +1,12 @@
+// This line of code is your worker's name tag.
+export default function AboutMe() {  
+    // This is the main part of the instructions.
+    // It tells the worker what to build on the webpage.
+    return (
+      <section>
+        <h2>About Me</h2>
+        <p>My name is Eduardo Lopez, but I prefer to go by Eddie. </p>
+        <p>I am new to building web stuff, but I am excited to learn new concepts and build cool projects.</p>
+      </section>
+    );
+  }
